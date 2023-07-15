a7790756e46a73dc4cf1e9de8e1f057991a5688a
Slava <dex252rus@gmail.com>
2023-07-15 14:53:20 +1000

    Вынести рещение из проектов, конвертировать консольное приложение в библиотеку

--- DelegatesSample/Extensions/IEnumerableExtension.cs
+++ DelegatesSample/Extensions/IEnumerableExtension.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections;
+using System.Diagnostics;
 
 namespace DelegatesSample.Extensions
 {
     public static class IEnumerableExtension
     {
-        public static T GetMax<T>(this IEnumerable e, Func<T, float> getParameter) 
+        public static T GetMax<T>(this IEnumerable e, Func<T, float> getParameter)
             where T : class
         {
             var dictionary = new Dictionary<T, float>();
@@ -16,10 +17,12 @@ namespace DelegatesSample.Extensions
 
             if (!dictionary.Any())
             {
+                Debug.WriteLine("Коллекция пустая, максимальный элемент равен null");
                 return null;
             }
 
             var max = dictionary.MaxBy(e => e.Value);
+            Debug.WriteLine($"Максимальный элемент найден, числовое значение элемента равно {max.Value}");
             return max.Key;
         }
     }
